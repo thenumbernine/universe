18df77425ce46db875416c3fe48d47ad4534205f
Christopher Moore <christopher.e.moore@gmail.com>
2017-12-07 00:09:08 +0300

    got all the pieces working

--- offline/process-fog-sdss3.lua
+++ offline/process-fog-sdss3.lua
@@ -9,19 +9,29 @@ local ImGuiApp = require 'imguiapp'
 local Orbit = require 'glapp.orbit'
 local View = require 'glapp.view'
 local gl = require 'gl'
+local ig = require 'ffi.imgui'
 local bit = require 'bit'
 local glreport = require 'gl.report'
+local GLProgram = require 'gl.program'
 
 local App = class(Orbit(View.apply(ImGuiApp)))
 App.title = 'FoG tool'
 App.viewDist = 2
 
-local buf = ffi.new'GLuint[1]'
+local glbuf = ffi.new'GLuint[1]'
 local n
 local cdata
 local env
-function App:initGL()
 
+function refreshPoints()
+	gl.glBindBuffer(gl.GL_ARRAY_BUFFER, glbuf[0])
+	gl.glBufferData(gl.GL_ARRAY_BUFFER, 3*n*ffi.sizeof'GLdouble', cdata, gl.GL_STATIC_DRAW)
+	gl.glBindBuffer(gl.GL_ARRAY_BUFFER, 0)
+end
+
+function App:initGL(...)
+	App.super.initGL(self, ...)
+	
 	-- init cl after gl so it has sharing
 	local data = file['datasets/sdss3/points/spherical.f64']	-- format is double z, ra, dec
 	n = #data / (3 * ffi.sizeof'double')
@@ -32,12 +42,12 @@ function App:initGL()
 	env = require 'cl.obj.env'{precision='double', size=n}
 
 	local real3code = [[
-	typedef union {
-		real s[3];
-		struct { real s0, s1, s2; };
-		struct { real x, y, z; };
-	} real3;
-	]]
+typedef union {
+	real s[3];
+	struct { real s0, s1, s2; };
+	struct { real x, y, z; };
+} real3;
+]]
 
 	ffi.cdef(real3code)
 	env.code = env.code .. real3code
@@ -52,26 +62,70 @@ function App:initGL()
 		]],
 	}
 	
-	gl.glGenBuffers(1, buf)
-	gl.glBindBuffer(gl.GL_ARRAY_BUFFER, buf[0])
-	gl.glBufferData(gl.GL_ARRAY_BUFFER, 3*n*ffi.sizeof'GLdouble', cdata, gl.GL_STATIC_DRAW)
-	gl.glVertexPointer(3, gl.GL_DOUBLE, 0, nil)
-	gl.glBindBuffer(gl.GL_ARRAY_BUFFER, 0)
+	gl.glGenBuffers(1, glbuf)
+	refreshPoints()
+	
+	sphereCoordShader = GLProgram{
+		vertexCode = [[
+varying vec4 color;
+void main() {
+	float z = gl_Vertex.x;
+	float rad_ra = gl_Vertex.y;
+	float rad_dec = gl_Vertex.z;
+
+	float cos_dec = cos(rad_dec);
+	vec4 vtx = vec4(
+		z * cos(rad_ra) * cos_dec,
+		z * sin(rad_ra) * cos_dec,
+		z * sin(rad_dec),
+		1.);
+	gl_Position = gl_ModelViewProjectionMatrix * vtx;
+	color = gl_Color;
+}
+]],
+		fragmentCode = [[
+varying vec4 color;
+void main() {
+	gl_FragColor = color;
+}
+]],
+	}
+	sphereCoordShader:useNone()
 	glreport'here'
 end
 
+local alpha = ffi.new('float[1]', 1)
+local pointSize = ffi.new('float[1]', 1)
 function App:update()
 	gl.glClear(bit.bor(gl.GL_DEPTH_BUFFER_BIT, gl.GL_COLOR_BUFFER_BIT))
+	
+	gl.glEnable(gl.GL_BLEND)
+	gl.glBlendFunc(gl.GL_SRC_ALPHA, gl.GL_ONE)
+	gl.glColor4f(1,1,1,alpha[0])
+	gl.glPointSize(pointSize[0])
+	gl.glEnable(gl.GL_POINT_SMOOTH)
+
 	gl.glEnableClientState(gl.GL_VERTEX_ARRAY)
-	gl.glBindBuffer(gl.GL_ARRAY_BUFFER, buf[0])
+	sphereCoordShader:use()
+	gl.glBindBuffer(gl.GL_ARRAY_BUFFER, glbuf[0])
+	gl.glVertexPointer(3, gl.GL_DOUBLE, 0, nil)	-- call every frame
+	
 	gl.glDrawArrays(gl.GL_POINTS, 0, n)
+	
 	gl.glBindBuffer(gl.GL_ARRAY_BUFFER, 0)
+	sphereCoordShader:useNone()
 	gl.glDisableClientState(gl.GL_VERTEX_ARRAY)
+	
+	gl.glPointSize(1)
+	gl.glDisable(gl.GL_BLEND)
+	
 	glreport'here'
 	App.super.update(self)
 end
 
 function App:updateGUI()
+	ig.igSliderFloat('size', pointSize, 1, 10)
+	ig.igSliderFloat('alpha', alpha, 0, 1, '%.3f', 10)
 end
 
 App():run()
